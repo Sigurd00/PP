29e498c59aee812ee59cf00d684092f71308231b
Sigurd <j.skadborg00@gmail.com>
2023-12-26 23:33:50 +0100

    session 2

---
+++ session2.hs
@@ -0,0 +1,14 @@
+import Control.Monad (when)
+second xs = xs !! 1
+
+allbutsecond (x:_:xs) = x : xs
+
+midtover xs = (take (length xs `div` 2) xs, drop (length xs `div` 2) xs)
+midtover2 xs = splitAt (length xs `div` 2) xs
+twoLists xs = (xs,xs)
+
+bingo x y = x `mod` z
+    where
+    z = y + 42
+
+final xs = head (reverse xs)
